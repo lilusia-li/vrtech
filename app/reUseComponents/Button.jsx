72959e262a958c10d78b4c9dcfafb7ca40f90d4a
XXX <el.kresik@gmail.com>
2026-02-05 14:51:13 +0300

    (Button) change gradient and size settings

--- app/reUseComponents/Button.jsx
+++ app/reUseComponents/Button.jsx
@@ -2,10 +2,10 @@ export default function Button(props) {
   const { style, children } = props;
   return (
     <button
-      className={`flex items-center justify-center w-full font-medium rounded-[20px] cursor-pointer text-[#ffffff] p-[0.8em] ${style}`}
+      className={`flex items-center justify-center font-medium rounded-[20px] cursor-pointer text-white p-[0.8em] ${style}`}
       style={{
         backgroundImage:
-          "linear-gradient(0turn,rgba(36,119,168,1) 0%,rgba(33,155,227,1) 100%)",
+          "linear-gradient(0turn,rgba(33,155,227,1) 0%,rgba(36,119,168,1) 100%)",
       }}
     >
       {children}
