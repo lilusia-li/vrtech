f1c835da192265ee7dcb3faa1e8225da0bd8b0be
XXX <el.kresik@gmail.com>
2026-02-12 22:57:22 +0800

    (AuditForm) create

--- app/[locale]/ai/page.jsx
+++ app/[locale]/ai/page.jsx
@@ -1,4 +1,5 @@
 import Advantages from "@/app/components/Advantages";
+import AuditForm from "@/app/components/AuditForm";
 import Cases from "@/app/components/Cases";
 import Clients from "@/app/components/Clients";
 import Hero from "@/app/components/Hero";
@@ -19,12 +20,15 @@ export default async function AiPage({ params }) {
       <Hero translations={t} currentLocale={currentLocale}></Hero>
       <Advantages translations={t}></Advantages>
       <Types translations={t} currentLocale={currentLocale}></Types>
+      <AuditForm translations={t}></AuditForm>
       <Stages translations={t}></Stages>
       <Speech translations={t}></Speech>
       <Numbers translations={t}></Numbers>
       <Clients translations={t}></Clients>
+      <AuditForm translations={t}></AuditForm>
       <Cases translations={t}></Cases>
       <Reviews translations={t}></Reviews>
+      <AuditForm translations={t}></AuditForm>
     </div>
   );
 }
---
+++ app/components/AuditForm.jsx
@@ -0,0 +1,11 @@
+export default function AuditForm({ translations }) {
+  return (
+    <section
+      className="h-[500px]"
+      style={{
+        backgroundImage:
+          "linear-gradient(0.439turn, rgba(223, 232, 255, 1) 0%, rgba(172, 198, 244, 1) 100%)",
+      }}
+    ></section>
+  );
+}
