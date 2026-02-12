dc1d96127575f68528163380773afc036ccd97bd
XXX <el.kresik@gmail.com>
2026-02-12 23:17:38 +0800

    (Speech) add bg-gradient and bg-robot

--- app/components/Speech.jsx
+++ app/components/Speech.jsx
@@ -1,13 +1,20 @@
 export default function Speech({ translations }) {
   const { speech: s } = translations;
   return (
-    <section className="px-8 py-[7rem]">
+    <section
+      className="px-8 py-[4rem] overflow-hidden"
+      style={{
+        backgroundImage:
+          "linear-gradient(0.742turn,rgba(172,198,244,1) 0%,rgba(223,232,255,0) 100%)",
+      }}
+    >
       {/* content container */}
-      <div className="grid grid-cols-2 gap-x-[2rem] max-w-[85rem] w-full m-auto">
+      <div className="relative flex justify-end gap-x-[2rem] max-w-[85rem] w-full m-auto">
         <img
           src="/speech/robot.svg"
           width={698}
-          className="justify-self-start"
+          className="absolute -left-5 -bottom-25 max-w-[698px] w-full"
+          aria-hidden
         />
         <div className="flex flex-col gap-[2rem] max-w-[35rem] justify-self-end">
           <h2 className="text-[2.5rem] font-medium first:mb-[1rem]">
